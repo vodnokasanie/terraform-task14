f8807e4ea2ccca41127016a3555f7d419efb5923
Torgen <118271296+vodnokasanie@users.noreply.github.com>
2025-11-02 12:12:22 +0600

    M

--- modules/application/main.tf
+++ modules/application/main.tf
@@ -152,3 +152,7 @@ resource "aws_autoscaling_group" "app_asg" {
 #   lb_target_group_arn    = aws_lb_target_group.app_tg_correct.arn
 # }
 
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.app_asg.name
+  alb_target_group_arn   = aws_lb_target_group.app_tg.arn
+}
